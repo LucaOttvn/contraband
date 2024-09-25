4928b83c2d3efadc349787234d0ba3d7d1f3c2d3
Lucatremila <lucatremila@gmail.com>
2024-09-25 16:32:47 +0200

    deleted skillsCreation

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import {localStorageItems, pagesNames, Player } from '../context';
-import SkillsCreation from './SkillsCreation';
-import { getCollection, updatePlayer } from '../../../utils/firestoreQueries';
-import ProfileOverview from './ProfileOverview';
 
 interface ProfileProps {
   player: Player
@@ -12,13 +9,6 @@ interface ProfileProps {
 
 export default function Profile(props: ProfileProps) {
 
-  const [updatingSkills, setUpdatingSkills] = useState<boolean>(false)
-
-  useEffect(() => {
-    // if there already are some skills saved go to the detail page, otherwise in the setup one
-    setUpdatingSkills(!props.player.skills?.length);
-  }, []);
-
   return (
     <div className='w-full h-full flex flex-col items-start gap-10'>
       <button className='generalButton' onClick={() => {
@@ -26,12 +16,7 @@ export default function Profile(props: ProfileProps) {
         props.setCurrentPage(pagesNames.login)
       }}>Logout</button>
 
-      {
-        updatingSkills ?
-          <SkillsCreation player={props.player} setPlayer={props.setPlayer} setUpdatingSkills={setUpdatingSkills}/> :
-          <ProfileOverview />
-      }
-
+   
     </div>
   );
 }
